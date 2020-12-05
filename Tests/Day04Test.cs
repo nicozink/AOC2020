1aee62880285f83fee4004d512290a3528a43504
nicozink <nico@nfzink.com>
2020-12-05 02:17:45 +0000

    Day 4 tests

---
+++ Tests/Day04Test.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day04Test
+    {
+        [TestMethod]
+        public void TestExample1()
+        {
+            var solution = new Solutions.Day04();
+
+            int result = solution.GetSolution1("Input/Day04/Example1.txt");
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestSolution1()
+        {
+            var solution = new Solutions.Day04();
+
+            int result = solution.GetSolution1("Input/Day04/Input.txt");
+
+            Assert.AreEqual(237, result);
+        }
+
+        [TestMethod]
+        public void TestExample2()
+        {
+            var solution = new Solutions.Day04();
+
+            long result = solution.GetSolution2("Input/Day04/Example2.txt");
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestSolution2()
+        {
+            var solution = new Solutions.Day04();
+
+            long result = solution.GetSolution2("Input/Day04/Input.txt");
+
+            Assert.AreEqual(172, result);
+        }
+    }
+}
