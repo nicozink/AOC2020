c00e089ed585f78d2b1b0bb500553befd0055632
Nico Zink <nico@zink.dev>
2020-12-13 12:45:44 +0000

    Day 13 solution

--- Day13/Day13.cs
+++ Day13/Day13.cs
@@ -12,14 +12,117 @@ namespace Solutions
     [Common.SolutionClass(Day = 13)]
     public class Day13
     {
-        public int GetSolution1(String path)
+        /// <summary>
+        /// Parses an input string to extract all bus ids.
+        /// An 'x' is converted to a 1, since those buses
+        /// are wildcards - so valid for any minute.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns>The bus ids.</returns>
+        IEnumerable<int> ReadBusIds(String input)
         {
-            return 0;
+            foreach (var id in input.Split(','))
+            {
+                if (id == "x")
+                {
+                    yield return 1;
+                }
+                else
+                {
+                    yield return int.Parse(id);
+                }
+            }
         }
 
+        /// <summary>
+        /// Finds the earliest bus after the given time stamp. The
+        /// time stamp is given from the first line. the bus ids are
+        /// read from the second line.
+        /// </summary>
+        /// <param name="path">The input file.</param>
+        /// <returns>The product of the earlest bus id and wait time.</returns>
+        public int GetSolution1(String path)
+        {
+            var lines = System.IO.File.ReadAllLines(path).ToList();
+
+            int timeStamp = int.Parse(lines[0]);
+            var busIds = lines[1]
+                .Split(',')
+                .Where(x => x != "x")
+                .Select(x => int.Parse(x));
+
+            // Find the earliest bus arrival with the shortest wait time.
+
+            int shortestWait = int.MaxValue;
+            int earliestBusID = 0;
+
+            foreach (var currentID in busIds)
+            {
+                // Find the divisor that gives us the previous bus
+                // arrival before the timestamp. We use that to
+                // calculate the next arrival time.
+                int previousBusFrequency = timeStamp / currentID;
+                int nextBusTime = currentID * (previousBusFrequency + 1);
+
+                // Use the current bus id if it is earlier than the previous bus id.
+                int waitTime = nextBusTime - timeStamp;
+                if (waitTime < shortestWait)
+                {
+                    shortestWait = waitTime;
+                    earliestBusID = currentID;
+                }
+            }
+
+            return shortestWait * earliestBusID;
+        }
+
+        /// <summary>
+        /// We find a time stamp, where all bus ids depart at subsequent
+        /// minutes in the order they were given.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns>The time stamp.</returns>
+        public long SolveSubsequentDepartures(String input)
+        {
+            var busIds = input
+                .Split(',')
+                .Select(x => x == "x" ? 1 : int.Parse(x))
+                .ToList();
+
+            long timeStamp = 0;
+            long delta = busIds[0];
+
+            for (int i = 1; i < busIds.Count; ++i)
+            {
+                // Scan through the timeline until we find a
+                // time stamp that is perfectly divisible by
+                // the bus id (with the position offset)
+                var busId = busIds[i];
+                while ((timeStamp + i) % busId != 0)
+                {
+                    timeStamp += delta;
+                }
+
+                // Include the bus id in the delta, to encode
+                // the current bus id in all future searches.
+                delta *= busId;
+            }
+
+            return timeStamp;
+        }
+
+        /// <summary>
+        /// Reads a file, and finds a starting point where
+        /// each bus departs at a subsequent time within a
+        /// minute each.
+        /// </summary>
+        /// <param name="path">The input path.</param>
+        /// <returns>The starting time stamp.</returns>
         public long GetSolution2(String path)
         {
-            return 0;
+            var lines = System.IO.File.ReadAllLines(path).ToList();
+
+            return SolveSubsequentDepartures(lines[1]);
         }
 
         [Common.SolutionMethod(Type = SolutionType.Main, Part = 1)]
--- Day13/Test.cs
+++ Day13/Test.cs
@@ -7,13 +7,36 @@ namespace Tests
     public class Day13Test
     {
         [TestMethod]
+        public void TestExample1()
+        {
+            var solution = new Solutions.Day13();
+
+            int result = solution.GetSolution1("Day13/Example.txt");
+
+            Assert.AreEqual(295, result);
+        }
+
+        [TestMethod]
         public void TestSolution1()
         {
             var solution = new Solutions.Day13();
 
             int result = solution.GetSolution1("Day13/Input.txt");
 
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(5257, result);
+        }
+
+        [TestMethod]
+        public void TestExample2()
+        {
+            var solution = new Solutions.Day13();
+
+            Assert.AreEqual(1068781, solution.SolveSubsequentDepartures("7,13,x,x,59,x,31,19"));
+            Assert.AreEqual(3417, solution.SolveSubsequentDepartures("17,x,13,19"));
+            Assert.AreEqual(754018, solution.SolveSubsequentDepartures("67,7,59,61"));
+            Assert.AreEqual(779210, solution.SolveSubsequentDepartures("67,x,7,59,61"));
+            Assert.AreEqual(1261476, solution.SolveSubsequentDepartures("67,7,x,59,61"));
+            Assert.AreEqual(1202161486, solution.SolveSubsequentDepartures("1789,37,47,1889"));
         }
 
         [TestMethod]
@@ -23,7 +46,7 @@ namespace Tests
 
             long result = solution.GetSolution2("Day13/Input.txt");
 
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(538703333547789, result);
         }
     }
 }
