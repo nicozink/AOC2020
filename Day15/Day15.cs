f3187559416e29a221d4c66c30d6ae5a1d189366
Nico Zink <nico@zink.dev>
2020-12-15 08:56:31 +0000

    Day 15 solution

--- Day15/Day15.cs
+++ Day15/Day15.cs
@@ -13,26 +13,60 @@ namespace Solutions
     [Common.SolutionClass(Day = 15)]
     public class Day15
     {
-        public long GetSolution1(String path)
+        /// <summary>
+        /// Gets the nth number, based on the starting numbers.
+        /// </summary>
+        /// <param name="starting">The starting numbers.</param>
+        /// <param name="n">The turn n.</param>
+        /// <returns>The nth number.</returns>
+        public int GetNthNumber(List<int> starting, int n)
         {
-            return 0;
-        }
+            // We process the starting numbers by putting them in a dictionary
+            // that stores the last turn. We exclude the last number, since that
+            // will need to be processed the same way we handle new numbers.
+            var history = starting.GetRange(0, starting.Count - 1)
+                .Select((num, index) => new Tuple<int, int>(num, index + 1))
+                .ToDictionary(tuple => tuple.Item1, tuple => tuple.Item2);
 
-        public long GetSolution2(String path)
-        {
-            return 0;
+            var lastNumber = starting.Last();
+            var lastTurn = starting.Count();
+
+            while (true)
+            {
+                var nextNumber = 0;
+
+                // We get the next number based on the last number, and
+                // the previous history based on the last turn.
+                if (history.ContainsKey(lastNumber))
+                {
+                    nextNumber = lastTurn - history[lastNumber];
+                }
+
+                // We needed the history of the last number before the
+                // last turn, but it's safe to update the hitory now.
+                history[lastNumber] = lastTurn;
+
+                var nextTurn = lastTurn + 1;
+                if (nextTurn == n)
+                {
+                    return nextNumber;
+                }
+
+                lastNumber = nextNumber;
+                lastTurn = nextTurn;
+            }
         }
 
         [Common.SolutionMethod(Type = SolutionType.Main, Part = 1)]
         public void Part1()
         {
-           Console.WriteLine("The answer is {0}", GetSolution1("Day15/Input.txt"));
+           Console.WriteLine("The answer is {0}", GetNthNumber(new List<int> { 2, 0, 1, 7, 4, 14, 18 }, 2020));
         }
 
         [Common.SolutionMethod(Part = 2)]
         public void Part2()
         {
-            Console.WriteLine("The answer is {0}", GetSolution2("Day15/Input.txt"));
+            Console.WriteLine("The answer is {0}", GetNthNumber(new List<int> { 2, 0, 1, 7, 4, 14, 18 }, 30000000));
         }
     }
 }
--- Day15/Test.cs
+++ Day15/Test.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -7,13 +8,41 @@ namespace Tests
     public class Day15Test
     {
         [TestMethod]
+        public void TestExample1()
+        {
+            var solution = new Solutions.Day15();
+
+            Assert.AreEqual(436, solution.GetNthNumber(new List<int> { 0, 3, 6 }, 2020));
+            Assert.AreEqual(1, solution.GetNthNumber(new List<int> { 1, 3, 2 }, 2020));
+            Assert.AreEqual(10, solution.GetNthNumber(new List<int> { 2, 1, 3 }, 2020));
+            Assert.AreEqual(27, solution.GetNthNumber(new List<int> { 1, 2, 3 }, 2020));
+            Assert.AreEqual(78, solution.GetNthNumber(new List<int> { 2, 3, 1 }, 2020));
+            Assert.AreEqual(438, solution.GetNthNumber(new List<int> { 3, 2, 1 }, 2020));
+            Assert.AreEqual(1836, solution.GetNthNumber(new List<int> { 3, 1, 2 }, 2020));
+        }
+
+        [TestMethod]
         public void TestSolution1()
         {
             var solution = new Solutions.Day15();
 
-            long result = solution.GetSolution1("Day15/Input.txt");
+            long result = solution.GetNthNumber(new List<int> { 2, 0, 1, 7, 4, 14, 18 }, 2020);
 
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(496, result);
+        }
+
+        [TestMethod]
+        public void TestExample2()
+        {
+            var solution = new Solutions.Day15();
+
+            Assert.AreEqual(175594, solution.GetNthNumber(new List<int> { 0, 3, 6 }, 30000000));
+            Assert.AreEqual(2578, solution.GetNthNumber(new List<int> { 1, 3, 2 }, 30000000));
+            Assert.AreEqual(3544142, solution.GetNthNumber(new List<int> { 2, 1, 3 }, 30000000));
+            Assert.AreEqual(261214, solution.GetNthNumber(new List<int> { 1, 2, 3 }, 30000000));
+            Assert.AreEqual(6895259, solution.GetNthNumber(new List<int> { 2, 3, 1 }, 30000000));
+            Assert.AreEqual(18, solution.GetNthNumber(new List<int> { 3, 2, 1 }, 30000000));
+            Assert.AreEqual(362, solution.GetNthNumber(new List<int> { 3, 1, 2 }, 30000000));
         }
 
         [TestMethod]
@@ -21,9 +50,9 @@ namespace Tests
         {
             var solution = new Solutions.Day15();
 
-            long result = solution.GetSolution2("Day15/Input.txt");
+            long result = solution.GetNthNumber(new List<int> { 2, 0, 1, 7, 4, 14, 18 }, 30000000);
 
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(883, result);
         }
     }
 }
