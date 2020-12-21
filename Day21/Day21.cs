ecdbb6583ed9bab5dc842ea6326a961cb328b624
nicozink <nico@nfzink.com>
2020-12-21 13:20:59 +0000

    Day 21 solution

--- Day21/Day21.cs
+++ Day21/Day21.cs
@@ -12,14 +12,174 @@ namespace Solutions
     [Common.SolutionClass(Day = 21)]
     public class Day21
     {
-        public long GetSolution1(String path)
+        /// <summary>
+        /// Stores a food item. This contains a list of ingredients,
+        /// and a list of allergens.
+        /// </summary>
+        class FoodItem
         {
-            return 0;
+            internal HashSet<string> Ingredients = new HashSet<string>();
+
+            internal HashSet<string> Allergens = new HashSet<string>();
         }
 
-        public long GetSolution2(String path)
+        /// <summary>
+        /// Reads the foods from the file.
+        /// </summary>
+        /// <param name="path">The file path</param>
+        /// <returns>The food items</returns>
+        private IEnumerable<FoodItem> ReadFoods(String path)
         {
-            return 0;
+            var input = System.IO.File.ReadLines(path);
+
+            foreach (var line in input)
+            {
+                var ingredientsAllergens = line
+                    .Replace(")", "")
+                    .Split(new string[] { " (contains " }, StringSplitOptions.None);
+
+                var ingredients = ingredientsAllergens[0]
+                    .Split()
+                    .ToHashSet();
+
+                var allergens = ingredientsAllergens[1]
+                    .Split(new string[] { ", " }, StringSplitOptions.None)
+                    .ToHashSet();
+
+                yield return new FoodItem()
+                {
+                    Ingredients = ingredients,
+                    Allergens = allergens
+                };
+            }
+        }
+
+        /// <summary>
+        /// Gets a lookup of the ingredients based on the allergen.
+        /// </summary>
+        /// <param name="foodItems">The food items.</param>
+        /// <returns>The lookup of possible ingredients for each allergen.</returns>
+        Dictionary<string, HashSet<string>> GetIngredientLookup(IEnumerable<FoodItem> foodItems)
+        {
+            var uniqueAllergens = new HashSet<string>(foodItems.SelectMany(x => x.Allergens));
+
+            var ingredientLoopup = new Dictionary<string, HashSet<string>>();
+            foreach (var allergen in uniqueAllergens)
+            {
+                HashSet<string> possibleIngredients = null;
+
+                // We go through each food where the allergen contains the specific food.
+                // Where there are more than one food containing the allergen, we take the
+                // intersection of the hash sets to filter the list of ingredients.
+                var allergenFoods = foodItems.Where(item => item.Allergens.Contains(allergen));
+                foreach (var food in allergenFoods)
+                {
+                    // If the possible ingredients is null, we take the first list
+                    // of ingredients we find to seed the list. Otherwise, we combine
+                    // the new list of ingredients with the previous one.
+                    if (possibleIngredients == null)
+                    {
+                        possibleIngredients = food.Ingredients.ToHashSet();
+                    }
+                    else
+                    {
+                        possibleIngredients.IntersectWith(food.Ingredients);
+                    }
+                }
+
+                ingredientLoopup.Add(allergen, possibleIngredients);
+            }
+
+            return ingredientLoopup;
+        }
+
+        /// <summary>
+        /// This function creates the lookup, and then counts the number of times that
+        /// ingredients don't contain any allergens.
+        /// </summary>
+        /// <param name="path">The input path.</param>
+        /// <returns>The number of times that ingredients don't add any allergens.</returns>
+        public long GetSolution1(String path)
+        {
+            var foodItems = ReadFoods(path);
+            var ingredientLoopup = GetIngredientLookup(foodItems);
+
+            var uniqueIngredients = new HashSet<string>(foodItems.SelectMany(x => x.Ingredients));
+
+            var unusedCount = 0;
+            foreach (var ingredient in uniqueIngredients)
+            {
+                // If ingredients aren't present for any of the allergens, then
+                // we add the number of times this ingredient is present to the
+                // total count.
+                if (!ingredientLoopup.Any(x => x.Value.Contains(ingredient)))
+                {
+                    unusedCount += foodItems.Count(item => item.Ingredients.Contains(ingredient));
+                }
+            }
+
+            return unusedCount;
+        }
+
+        /// <summary>
+        /// This figures out a pairing between the allergens and ingredients, and produces
+        /// a list of ingredients based on the sorted list of allergens.
+        /// </summary>
+        /// <param name="path">The input file.</param>
+        /// <returns>The list of ingredients.</returns>
+        public string GetSolution2(String path)
+        {
+            var foodItems = ReadFoods(path);
+            var ingredientLoopup = GetIngredientLookup(foodItems);
+
+            // In this loop, we repeatedly look at the ingredient lookup.
+            // and ingredient that is matched to exactly one allergen is
+            // considered "locked" in, so we look for other allergens and
+            // remove locked in ingredients from their list of possibilities.
+            while (ingredientLoopup.Any(x => x.Value.Count != 1))
+            {
+                // These are all allergens that still have multiple
+                // options for an ingredient.
+                var manyAllergens = ingredientLoopup
+                    .Where(x => x.Value.Count != 1)
+                    .ToList();
+
+                foreach (var lookup in manyAllergens)
+                {
+                    // We go through each ingredient in the list.
+                    foreach (var ingredient in lookup.Value.ToList())
+                    {
+                        // We look to see if any ingredient in this list
+                        // is present as a locked in ingredient for another
+                        // allergen.
+                        var hasOtherUniqueUse = ingredientLoopup
+                            .Any(x => x.Key != lookup.Key &&
+                                x.Value.Count == 1 &&
+                                x.Value.Contains(ingredient));
+
+                        // If it is, we remove it from the list.
+                        if (hasOtherUniqueUse)
+                        {
+                            lookup.Value.Remove(ingredient);
+                        }
+                    }
+                }
+            }
+
+            // We now have a one-to-one mapping between ingredients and
+            // allergens. So we sort by allergen, and create a list of ingredients.
+
+            var uniqueList = ingredientLoopup
+                .OrderBy(x => x.Key)
+                .Select(x => x.Value.First());
+
+            var returnString = "";
+            foreach (var item in uniqueList)
+            {
+                returnString += item + ",";
+            }
+
+            return returnString.Substring(0, returnString.Length - 1);
         }
 
         [Common.SolutionMethod(Type = SolutionType.Main, Part = 1)]
--- Day21/Test.cs
+++ Day21/Test.cs
@@ -7,13 +7,33 @@ namespace Tests
     public class Day21Test
     {
         [TestMethod]
+        public void TestExample1()
+        {
+            var solution = new Solutions.Day21();
+
+            var result = solution.GetSolution1("Day21/Example.txt");
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
         public void TestSolution1()
         {
             var solution = new Solutions.Day21();
 
             var result = solution.GetSolution1("Day21/Input.txt");
 
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(2584, result);
+        }
+
+        [TestMethod]
+        public void TestExample2()
+        {
+            var solution = new Solutions.Day21();
+
+            var result = solution.GetSolution2("Day21/Example.txt");
+
+            Assert.AreEqual("mxmxvkd,sqjhc,fvjkl", result);
         }
 
         [TestMethod]
@@ -23,7 +43,7 @@ namespace Tests
 
             var result = solution.GetSolution2("Day21/Input.txt");
 
-            Assert.AreEqual(0, result);
+            Assert.AreEqual("fqhpsl,zxncg,clzpsl,zbbnj,jkgbvlxh,dzqc,ppj,glzb", result);
         }
     }
 }
