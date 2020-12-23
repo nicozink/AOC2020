a7a1905904d0074d9d796fac14e5d240aae742ce
nicozink <nico@nfzink.com>
2020-12-23 12:54:38 +0000

    Day 24 prep

---
+++ Day24/Day24.cs
@@ -0,0 +1,37 @@
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
+{
+    /// <summary>
+    /// Solution for day 24:
+    /// https://adventofcode.com/2020/day/24
+    /// </summary>
+    [Common.SolutionClass(Day = 24)]
+    public class Day24
+    {
+        public long GetSolution1(String path)
+        {
+            return 0;
+        }
+
+        public long GetSolution2(String path)
+        {
+            return 0;
+        }
+
+        [Common.SolutionMethod(Type = SolutionType.Main, Part = 1)]
+        public void Part1()
+        {
+           Console.WriteLine("The answer is {0}", GetSolution1("Day24/Input.txt"));
+        }
+
+        [Common.SolutionMethod(Part = 2)]
+        public void Part2()
+        {
+            Console.WriteLine("The answer is {0}", GetSolution2("Day24/Input.txt"));
+        }
+    }
+}
