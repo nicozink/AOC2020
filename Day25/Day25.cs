cbb37e97ee1c042f6bd6af4bc80a3385e43bf988
nicozink <nico@nfzink.com>
2020-12-25 11:03:35 +0000

    Merry Christmas!

--- Day25/Day25.cs
+++ Day25/Day25.cs
@@ -12,15 +12,64 @@ namespace Solutions
     [Common.SolutionClass(Day = 25)]
     public class Day25
     {
-        public long GetSolution(String path)
+        /// <summary>
+        /// Transforms the value based on the subject number.
+        /// </summary>
+        /// <param name="value">The starting value.</param>
+        /// <param name="subjectNumber">The subject number.</param>
+        /// <returns>The transformed value.</returns>
+        private long Transform(long value, long subjectNumber)
         {
-            return 0;
+            long product = value * subjectNumber;
+            value = product % 20201227;
+
+            return value;
+        }
+
+        /// <summary>
+        /// Gets the loop size for the public key provided.
+        /// </summary>
+        /// <param name="publicKey">The public key.</param>
+        /// <returns>The loop size.</returns>
+        private int GetLoopSize(int publicKey)
+        {
+            int loopCount = 0;
+            long value = 1;
+
+            while (value != publicKey)
+            {
+                value = Transform(value, 7);
+
+                ++loopCount;
+            }
+
+            return loopCount;
+        }
+
+        /// <summary>
+        /// Calculates the encryption key based on the two
+        /// public keys provided.
+        /// </summary>
+        /// <param name="publicKey1">The first public key.</param>
+        /// <param name="publicKey2">The second public key.</param>
+        /// <returns>The encryption key.</returns>
+        public long GetSolution(int publicKey1, int publicKey2)
+        {
+            int loopSize1 = GetLoopSize(publicKey1);
+
+            long encryptionKey = 1;
+            for (int i = 0; i < loopSize1; ++i)
+            {
+                encryptionKey = Transform(encryptionKey, publicKey2);
+            }
+
+            return encryptionKey;
         }
 
         [Common.SolutionMethod(Type = SolutionType.Main, Part = 1)]
         public void Part1()
         {
-           Console.WriteLine("The answer is {0}", GetSolution("Day25/Input.txt"));
+           Console.WriteLine("The answer is {0}", GetSolution(18499292, 8790390));
         }
     }
 }
--- Day25/Test.cs
+++ Day25/Test.cs
@@ -7,13 +7,23 @@ namespace Tests
     public class Day25Test
     {
         [TestMethod]
+        public void TestExample()
+        {
+            var solution = new Solutions.Day25();
+
+            var result = solution.GetSolution(5764801, 17807724);
+
+            Assert.AreEqual(14897079, result);
+        }
+
+        [TestMethod]
         public void TestSolution()
         {
             var solution = new Solutions.Day25();
 
-            var result = solution.GetSolution("Day25/Input.txt");
+            var result = solution.GetSolution(18499292, 8790390);
 
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(18433997, result);
         }
     }
 }
